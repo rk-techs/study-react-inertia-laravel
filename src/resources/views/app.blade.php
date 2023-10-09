6d303df29c5719eee66913a62f9352ee290cbdd2
rk-techs <ryskdevelop@gmail.com>
2023-10-09 16:54:18 +0900

    enable sass with Vite

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
+    @vite(['resources/js/app.jsx', 'resources/sass/style.scss'])
     @inertiaHead
   </head>
   <body>
--- src/vite.config.js
+++ src/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        laravel(['resources/js/app.jsx']),
+        laravel([
+            'resources/js/app.jsx',
+            'resources/sass/style.scss',
+        ]),
         react(),
     ],
     server: {
