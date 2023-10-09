33b8eb1485a55a5f982ea5f0cdcfa6270cfd13eb
rk-techs <ryskdevelop@gmail.com>
2023-10-09 13:20:47 +0900

    chore: update vite config for React

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        laravel(['resources/js/app.js',]),
+        laravel(['resources/js/app.jsx']),
+        react(),
     ],
 });
